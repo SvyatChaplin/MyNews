861f25a1f0c354dc99d31e2f25a10334e0233366
Svyatoslav <svyat12.93@gmail.com>
2019-11-02 08:53:38 +0200

    initial commit

---
+++ MyNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNews
+//
+//  Created by Svyat Chaplin on 02.11.2019.
+//  Copyright © 2019 Svyat Chaplin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
